bc39a81fd37a4acaf33fc4130ee00068a50b5d95
AlmogBaku <almog.baku@gmail.com>
2016-09-20 00:42:59 +0300

    change the mapping to the new flat structure

--- common/elasticsearch/mapping.go
+++ common/elasticsearch/mapping.go
@@ -13,16 +13,41 @@
 // limitations under the License.
 package elasticsearch
 
-const mapping = `{
-  "aliases": {
-    "heapster-events": {
+import "strings"
 
-    }
-  },
-  "mappings": {
-    "k8s-heapster": {
+func metricTypeSchema(typeName string, metrics ...string) string {
+	metricSchemas := []string{}
+	for _, metricName := range metrics {
+		metricSchemas = append(metricSchemas,
+			`"`+metricName+`": {
+  "properties": {
+    "MetricsValue": {
       "properties": {
-        "MetricsName": {
+        "value": {
+  	"type": "long"
+        }
+      }
+    }
+  }
+}`,
+		)
+	}
+
+	return customMetricTypeSchema(typeName,
+		`"Metrics": {
+  "properties": {
+  `+strings.Join(metricSchemas, ",\r\n")+`
+  }
+}`,
+	)
+}
+
+func customMetricTypeSchema(typeName string, customSchema string) string {
+	return `"` + typeName + `": {
+  "properties": {
+    "MetricsTags": {
+      "properties": {
+        "container_base_image": {
           "type": "string",
           "index": "analyzed",
           "fields": {
@@ -32,111 +57,134 @@ const mapping = `{
             }
           }
         },
-        "MetricsTags": {
-          "properties": {
-            "container_base_image": {
-              "type": "string",
-              "index": "analyzed",
-              "fields": {
-                "raw": {
-                  "type": "string",
-                  "index": "not_analyzed"
-                }
-              }
-            },
-            "container_name": {
-              "type": "string",
-              "index": "analyzed",
-              "fields": {
-                "raw": {
-                  "type": "string",
-                  "index": "not_analyzed"
-                }
-              }
-            },
-            "host_id": {
-              "type": "string",
-              "index": "not_analyzed"
-            },
-            "hostname": {
-              "type": "string",
-              "index": "analyzed",
-              "fields": {
-                "raw": {
-                  "type": "string",
-                  "index": "not_analyzed"
-                }
-              }
-            },
-            "labels": {
-              "type": "string",
-              "index": "analyzed",
-              "fields": {
-                "raw": {
-                  "type": "string",
-                  "index": "not_analyzed"
-                }
-              }
-            },
-            "namespace_id": {
-              "type": "string",
-              "index": "not_analyzed"
-            },
-            "namespace_name": {
-              "type": "string",
-              "index": "not_analyzed"
-            },
-            "nodename": {
-              "type": "string",
-              "index": "analyzed",
-              "fields": {
-                "raw": {
-                  "type": "string",
-                  "index": "not_analyzed"
-                }
-              }
-            },
-            "pod_id": {
-              "type": "string",
-              "index": "not_analyzed"
-            },
-            "pod_name": {
-              "type": "string",
-              "index": "analyzed",
-              "fields": {
-                "raw": {
-                  "type": "string",
-                  "index": "not_analyzed"
-                }
-              }
-            },
-            "pod_namespace": {
-              "type": "string",
-              "index": "not_analyzed"
-            },
-            "resource_id": {
-              "type": "string",
-              "index": "not_analyzed"
-            },
-            "type": {
+        "container_name": {
+          "type": "string",
+          "index": "analyzed",
+          "fields": {
+            "raw": {
               "type": "string",
               "index": "not_analyzed"
             }
           }
         },
-        "MetricsTimestamp": {
-          "type": "date",
-          "format": "strict_date_optional_time||epoch_millis"
+        "host_id": {
+          "type": "string",
+          "index": "not_analyzed"
         },
-        "MetricsValue": {
-          "properties": {
-            "value": {
-              "type": "long"
+        "hostname": {
+          "type": "string",
+          "index": "analyzed",
+          "fields": {
+            "raw": {
+              "type": "string",
+              "index": "not_analyzed"
             }
           }
+        },
+        "labels": {
+          "type": "string",
+          "index": "analyzed",
+          "fields": {
+            "raw": {
+              "type": "string",
+              "index": "not_analyzed"
+            }
+          }
+        },
+        "namespace_id": {
+          "type": "string",
+          "index": "not_analyzed"
+        },
+        "namespace_name": {
+          "type": "string",
+          "index": "not_analyzed"
+        },
+        "nodename": {
+          "type": "string",
+          "index": "analyzed",
+          "fields": {
+            "raw": {
+              "type": "string",
+              "index": "not_analyzed"
+            }
+          }
+        },
+        "pod_id": {
+          "type": "string",
+          "index": "not_analyzed"
+        },
+        "pod_name": {
+          "type": "string",
+          "index": "analyzed",
+          "fields": {
+            "raw": {
+              "type": "string",
+              "index": "not_analyzed"
+            }
+          }
+        },
+        "pod_namespace": {
+          "type": "string",
+          "index": "not_analyzed"
+        },
+        "resource_id": {
+          "type": "string",
+          "index": "not_analyzed"
+        },
+        "type": {
+          "type": "string",
+          "index": "not_analyzed"
         }
       }
     },
+    "MetricsTimestamp": {
+      "type": "date",
+      "format": "strict_date_optional_time||epoch_millis"
+    },
+    ` + customSchema + `
+  }
+}`
+}
+
+var mapping = `{
+  "aliases": {
+    "heapster-cpu": {},
+    "heapster-filesystem": {},
+    "heapster-memory": {},
+    "heapster-network": {},
+    "heapster-general": {},
+    "heapster-events": {}
+  },
+  "mappings": {
+    ` + metricTypeSchema("cpu",
+	"limit", "node_capacity", "node_reservation", "node_utilization", "request", "usage", "usage_rate",
+) + `,
+    ` + metricTypeSchema("filesystem", "usage", "limit", "available") + `,
+    ` + metricTypeSchema("memory",
+	"limit", "major_page_faults", "major_page_faults_rate", "node_capacity", "node_reservation",
+	"node_utilization", "page_faults", "page_faults_rate", "request", "uasge", "working_set",
+) + `,
+    ` + metricTypeSchema("network",
+	"rx", "rx_errors", "rx_errors_rate", "rx_rate", "tx", "tx_errors", "tx_errors_rate", "tx_rate",
+) + `,
+    ` + metricTypeSchema("general",
+	`"MetricsName": {
+  "type": "string",
+  "index": "analyzed",
+  "fields": {
+    "raw": {
+      "type": "string",
+      "index": "not_analyzed"
+    }
+  },
+  "MetricsValue": {
+    "properties": {
+      "value": {
+        "type": "long"
+      }
+    }
+  }`) + `,
+
     "events": {
       "properties": {
         "EventTags": {
